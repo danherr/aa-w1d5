7aca5e2da1fe22a4ba01f1dd214420c138c4d2c4
Dan Herr && Nick Alzapiedi <emails@appacademy.io>
2015-12-04 11:46:05 -0500

    Add Board#neighbor_positions and Board#in_bounds

--- minesweeper/board.rb
+++ minesweeper/board.rb
@@ -1,6 +1,9 @@
 require_relative 'tile.rb'
 
 class Board
+
+  RELATIVE_NEIGHBORS = [[-1, -1],[1, 1],[-1,1],[1,-1],[0,1],[0,-1],[1,0],[-1,0]]
+
 attr_accessor :grid
 attr_reader :height, :width, :bomb_count
   def initialize(height = 9, width = 9, bomb_count = 10)
@@ -32,5 +35,23 @@ attr_reader :height, :width, :bomb_count
     end
 
   end
+
+  def neighbor_positions(position)
+    all_neighbors = RELATIVE_NEIGHBORS.map do |rel_pos|
+      x,y = position
+      dx, dy = rel_pos
+      [x + dx, y + dy]
+    end
+
+    all_neighbors.select {|pos| in_bounds(pos)}
+  end
+
+  def in_bounds(position)
+    x,y = position
+
+    x >= 0 && y >= 0 && x < height && y < width
+  end
+
+
 
 end
